39c5b4d525725389b0aa6db5420931432ae2d0ee
hamidrezakhodajou <42764887+hamidrezakhodajou@users.noreply.github.com>
2022-01-19 13:39:39 +0330

    update Network.py

--- Network.py
+++ Network.py
@@ -1,19 +1,19 @@
 import numpy as np
-from keras.layers import Input, Conv2D, MaxPooling2D, Conv2DTranspose, concatenate, BatchNormalization, Activation, add
-from keras.models import Model, model_from_json
-from keras import backend as K 
+from keras.layers import Input, Conv2D, MaxPooling2D, Conv2DTranspose, concatenate, Activation, add
+from keras.models import Model
 
-def conv2d_bn(x, filters, num_row, num_col, padding='same', strides=(1, 1), activation='relu', name=None):
-    x = Conv2D(filters, (num_row, num_col), strides=strides, padding=padding, use_bias=False, kernel_initializer='glorot_uniform')(x)
+
+def conv2d(x, filters, row_kernel, col_kernel, padding='same', strides=(1, 1), activation='relu', name=None):
+    x = Conv2D(filters, (row_kernel, col_kernel), strides=strides, padding=padding, use_bias=False, kernel_initializer='glorot_uniform')(x)
     if(activation == None):
         return x
     x = Activation(activation, name=name)(x)
     return x
 
-def MRF(W, inp):
-    shortcut = conv2d_bn(inp, 2*W, 1, 1, activation=None, padding='same')
-    conv3x3 = conv2d_bn(inp, W, 3, 3,activation='relu', padding='same')
-    conv5x5 = conv2d_bn(conv3x3, W, 3, 3, activation='relu', padding='same')
+def MRF(filters, inp):
+    shortcut = conv2d(inp, 2*filters, 1, 1, activation=None, padding='same')
+    conv3x3 = conv2d(inp, filters, 3, 3,activation='relu', padding='same')
+    conv5x5 = conv2d(conv3x3, filters, 3, 3, activation='relu', padding='same')
 
     out = concatenate([conv3x3, conv5x5], axis=3)
     out = add([shortcut, out])
@@ -25,64 +25,59 @@ def DenseSkipConnection(filters, length, inp, kernelSize):
     if length<=0:
         return inp
     elif length>0:
-        out = conv2d_bn(inp, filters, kernelSize, kernelSize, activation='relu', padding='same')
+        out = conv2d(inp, filters, kernelSize, kernelSize, activation='relu', padding='same')
         inp = concatenate([out, inp], axis=3)
     
         for i in range(length-1):
-            out = conv2d_bn(inp, filters, kernelSize, kernelSize, activation='relu', padding='same')
+            out = conv2d(inp, filters, kernelSize, kernelSize, activation='relu', padding='same')
             inp = concatenate([out, inp], axis=3)
         return inp
 
                
 
-def MRDC_CNN(height, width, n_channels, filters=16, kernelSize=3, base_Layer=4):
+def MRDC_CNN(height, width, n_channels, base_filter=16, kernelSize=3, base_Layer=4):
 
     inputs = Input((height, width, n_channels))
 
-    mresblock1 = MRF(filters, inputs)
-    pool1 = MaxPooling2D(pool_size=(2, 2))(mresblock1)
-    mresblock1 = DenseSkipConnection(filters, base_Layer, mresblock1, kernelSize)
+    MRFblock1 = MRF(base_filter, inputs)
+    pool1 = MaxPooling2D(pool_size=(2, 2))(MRFblock1)
+    MRFblock1 = DenseSkipConnection(base_filter, base_Layer, MRFblock1, kernelSize)
 
-    mresblock2 = MRF(filters*2, pool1)
-    pool2 = MaxPooling2D(pool_size=(2, 2))(mresblock2)
-    mresblock2 = DenseSkipConnection(filters*2, base_Layer-1, mresblock2, kernelSize)
+    MRFblock2 = MRF(base_filter*2, pool1)
+    pool2 = MaxPooling2D(pool_size=(2, 2))(MRFblock2)
+    MRFblock2 = DenseSkipConnection(base_filter*2, base_Layer-1, MRFblock2, kernelSize)
 
-    mresblock3 = MRF(filters*4, pool2)
-    pool3 = MaxPooling2D(pool_size=(2, 2))(mresblock3)
-    mresblock3 = DenseSkipConnection(filters*4, base_Layer-2, mresblock3, kernelSize)
+    MRFblock3 = MRF(base_filter*4, pool2)
+    pool3 = MaxPooling2D(pool_size=(2, 2))(MRFblock3)
+    MRFblock3 = DenseSkipConnection(base_filter*4, base_Layer-2, MRFblock3, kernelSize)
 
-    mresblock4 = MRF(filters*8, pool3)
-    pool4 = MaxPooling2D(pool_size=(2, 2))(mresblock4)
-    mresblock4 = DenseSkipConnection(filters*8, base_Layer-3, mresblock4, kernelSize)
+    MRFblock4 = MRF(base_filter*8, pool3)
+    pool4 = MaxPooling2D(pool_size=(2, 2))(MRFblock4)
+    MRFblock4 = DenseSkipConnection(base_filter*8, base_Layer-3, MRFblock4, kernelSize)
 
-    mresblock5 = MRF(filters*16, pool4)
+    MRFblock5 = MRF(base_filter*16, pool4)
 
     up6 = concatenate([Conv2DTranspose(
-        filters*8, (2,2), strides=(2, 2), padding='same', kernel_initializer='glorot_uniform')(mresblock5), mresblock4], axis=3)
-    mresblock6 = MRF(filters*8, up6)
+        base_filter*8, (2,2), strides=(2, 2), padding='same', kernel_initializer='glorot_uniform')(MRFblock5), MRFblock4], axis=3)
+    MRFblock6 = MRF(base_filter*8, up6)
 
     up7 = concatenate([Conv2DTranspose(
-        filters*4, (2,2), strides=(2, 2), padding='same', kernel_initializer='glorot_uniform')(mresblock6), mresblock3], axis=3)
-    mresblock7 = MRF(filters*4, up7)
+        base_filter*4, (2,2), strides=(2, 2), padding='same', kernel_initializer='glorot_uniform')(MRFblock6), MRFblock3], axis=3)
+    MRFblock7 = MRF(base_filter*4, up7)
 
     up8 = concatenate([Conv2DTranspose(
-        filters*2, (2,2), strides=(2, 2), padding='same', kernel_initializer='glorot_uniform')(mresblock7), mresblock2], axis=3)
-    mresblock8 = MRF(filters*2, up8)
+        base_filter*2, (2,2), strides=(2, 2), padding='same', kernel_initializer='glorot_uniform')(MRFblock7), MRFblock2], axis=3)
+    MRFblock8 = MRF(base_filter*2, up8)
 
     up9 = concatenate([Conv2DTranspose(
-        filters, (2,2), strides=(2, 2), padding='same', kernel_initializer='glorot_uniform')(mresblock8), mresblock1], axis=3)
-    mresblock9 = MRF(filters, up9)
+        base_filter, (2,2), strides=(2, 2), padding='same', kernel_initializer='glorot_uniform')(MRFblock8), MRFblock1], axis=3)
+    MRFblock9 = MRF(base_filter, up9)
     
     
-    conv10 = conv2d_bn(mresblock9, 1,1,1, activation='linear',name= '512') #512
-    conv11 = conv2d_bn(mresblock8, 1,1,1, activation='linear', name= '256') #256
-    conv12 = conv2d_bn(mresblock7, 1,1,1, activation='linear', name= '128') #128
-    conv13 = conv2d_bn(mresblock6, 1,1,1, activation='linear', name= '64') #64
+    conv10 = conv2d(MRFblock9, 1,1,1, activation='linear',name= '512') #512
+    conv11 = conv2d(MRFblock8, 1,1,1, activation='linear', name= '256') #256
+    conv12 = conv2d(MRFblock7, 1,1,1, activation='linear', name= '128') #128
+    conv13 = conv2d(MRFblock6, 1,1,1, activation='linear', name= '64') #64
 
     model = Model(inputs=[inputs], outputs=[conv10, conv11, conv12, conv13])
     return model
-
-
-
-
-
